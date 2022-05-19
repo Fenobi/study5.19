f65047dce0c5e5e55ded1f1d40e875494469dde7
Fenobi <13667739122@qq.com>
2022-05-19 23:13:11 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int t = 0;
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (int i = 0; i < 10; i++)
+//	{
+//		t = arr[i];
+//		arr[i] = arr[9 - i];
+//		arr[9 - i] = t;
+//	}
+//	for (int i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 0 };
+//	int max = arr[0];
+//	int t = 0;
+//	for(i=0;i<10;i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	for (i = 1; i < 10; i++)
+//	{
+//		if (max < arr[i])
+//		{
+//			max = arr[i];
+//			t = i;
+//		}
+//	}
+//	printf("max=%d ÏÂ±êÎª£º%d\n", max, t);
+//	return 0;
+//}
+//
+//int main()
+//{
+//
+//	return 0;
+//}
